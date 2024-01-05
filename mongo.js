4c13b0032856e41d17635b9c25ddd1f81819134e
dileepadari <adaridileep@gmail.com>
2024-01-06 01:03:49 +0530

    Project Added

---
+++ mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+
+
+
+mongoose.connect(
+    `mongodb+srv://${username}:${password}@cluster0.2asz5h9.mongodb.net/users?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true,                                                                                                                                                                                                                                                         
+        useUnifiedTopology: true,
+    }
+).then(()=>{
+    console.log("Database connection successfull");
+})
+.catch(()=>{
+    console.log("Database failed to connect");
+});
+
+
+
+
+
+const UserSchema = new mongoose.Schema({
+    name: {
+      type:String,
+      required: true
+    },
+    email: {
+      type:String,
+      required: true
+    },
+    password: {
+      type:String,
+      required: true
+    },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports=User
